da254dd2a880fbf631e518599fcaccdf60ce583e
Apoorv Goyal <apoorvgoyal1212@gmail.com>
2024-05-11 14:39:27 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateAccountStep1 from './pages/Login/Create-account-step1';
+
+function ComponentName() {
+  return (
+    <div>
+      <CreateAccountStep1 />
+    </div>
+  );
+}
+
+export default ComponentName;
