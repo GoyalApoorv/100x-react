3537197278fa4f51a82e341676f7abe444a92a34
Apoorv Goyal <apoorvgoyal1212@gmail.com>
2024-06-21 16:15:08 +0530

    merge conflict main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -9,15 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>,
 );
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 50eb6ff7d92292f767d8dd9b3a89dac1ce90b2b4
