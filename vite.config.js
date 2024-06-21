15ba6e5818c9deb8f4daf94e9fc2ff33024f65a6
Apoorv Goyal <apoorvgoyal1212@gmail.com>
2024-06-21 16:07:33 +0530

    vite merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -11,14 +10,3 @@ export default defineConfig({
     },
   },
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
->>>>>>> 50eb6ff7d92292f767d8dd9b3a89dac1ce90b2b4
